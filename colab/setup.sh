4a261ccec5b545edb6659d12658c962e1b4e1600
Naoyuki Tamura <tamura@kobe-u.ac.jp>
2019-08-09 13:59:07 +0900

    new file:   colab/setup.sh

---
+++ colab/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p /opt/bin
+wget http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-knuth-linux64-bin.zip
+unzip taocpsat-knuth-linux64-bin.zip
+mv ~/knuth/bin/* /opt/bin/
+chmod +x /opt/bin/*
+
+wget http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-prog.zip
+unzip taocpsat-prog.zip
+
+apt install openjdk-8-jre-headless
+apt install scala
+
+cd prog
+mkdir target
+scalac -d target/taocpsat-1.0.jar src/main/scala/*.scala
+
+exit 0
+
