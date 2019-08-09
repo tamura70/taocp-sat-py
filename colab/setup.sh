31cbf42dd19558bfdb9b6f3002e8b0dc769e1fde
Naoyuki Tamura <tamura@kobe-u.ac.jp>
2019-08-09 15:18:04 +0900

    modified:   colab/setup.sh 	modified:   notebooks/index.ipynb 	modified:   notebooks/index.org

--- colab/setup.sh
+++ colab/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /content
 rm -r taocpsat-*.zip knuth prog
-wget http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-knuth-linux64-bin.zip
+wget -nc http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-knuth-linux64-bin.zip
 unzip taocpsat-knuth-linux64-bin.zip
-wget http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-prog.zip
+wget -nc http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-prog.zip
 unzip taocpsat-prog.zip
 
 mkdir -p /opt/bin
