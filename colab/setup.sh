6e775a9142f76248602460095c65927535d0a126
Naoyuki Tamura <tamura@kobe-u.ac.jp>
2023-12-02 23:36:07 +0900

    modified:   colab/setup.sh

--- colab/setup.sh
+++ colab/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd /content
 rm -r taocpsat-*.zip knuth prog
-wget -nc --quiet http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-knuth-linux64-bin.zip
+wget -nc --quiet https://taocp-fun.gitlab.io/v4f6-sat-tut/taocpsat-knuth-linux64-bin.zip
+
 unzip -q taocpsat-knuth-linux64-bin.zip
-wget -nc --quiet http://bach.istc.kobe-u.ac.jp/lect/taocp-sat/taocpsat-prog.zip
+wget -nc --quiet https://taocp-fun.gitlab.io/v4f6-sat-tut/taocpsat-prog.zip
 unzip -q taocpsat-prog.zip
 
 mkdir -p /opt/bin
